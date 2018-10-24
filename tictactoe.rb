e36c2375f6c8b2ca4faa5fe0e0312ac51140e64e
Jon Levin <jonathan.i.levin@gmail.com>
2018-10-24 16:53:35 -0400

    initial commit

---
+++ tictactoe.rb
@@ -0,0 +1,71 @@
+include Enumerable
+
+# initialize empty board, set current player as X, and start game
+$board = Array.new(3) {Array.new(3, " ")}
+current_player = "X"
+game = true
+
+def print_board(game)
+    rows = 0
+    print "\n  A   B   C \n"
+    game.each do |x|
+        columns = 0
+        print (rows +1)
+        x.each do |i|
+            print " " + i + " "
+            print "|" if columns != 2
+            columns += 1
+        end
+        if rows != 2 then print "\n --- --- ---\n" else print "\n" end
+        rows +=1
+    end
+    print "\n"
+end
+
+def make_a_move(player)
+    puts "Make your move player " + player + "."
+    move = gets.chomp
+    # check if placement is available
+    while move.length != 2 || $board[move[1].to_i - 1][((move[0].downcase.ord) - 97)] != " "
+        puts "Invalid move, try again. Ex: A1"
+        move = gets.chomp
+    end
+    $board[move[1].to_i - 1][(move[0].downcase.ord - 97)] = player
+end
+
+while game == true do
+    # print current game board and make a move
+    print_board($board)
+    make_a_move(current_player)
+
+    # check for vertical or horizontal wins
+    0.upto(2) do |i|
+        if $board[i][0] == current_player && $board[i][0] == $board[i][1] && $board[i][1] == $board[i][2] ||
+            $board[0][i] == current_player && $board[0][i] == $board[1][i] && $board[1][i] == $board[2][i]
+            print_board($board)
+            puts "YOU WIN, PLAYER " + current_player + "!\n\n"
+            game = false
+        end
+    end
+    # check for diagonal wins
+    if $board[0][0] == current_player && $board[0][0] == $board[1][1] && $board[1][1] == $board[2][2] ||
+        $board[0][2] == current_player && $board[0][2] == $board[1][1] && $board[1][1] == $board[2][0]
+        print_board($board)
+        puts "YOU WIN, PLAYER " + current_player + "!\n\n"
+        game = false
+    end
+
+    # check for a full board / tie
+    if ($board.all? { |i| i.all? { |x| x != " "}} == true)
+        print_board($board)    
+        puts "A TIE (no more moves)!\n\n"
+        game = false
+    end
+
+    # change players
+    if current_player == "X"
+        current_player = "O"
+    else
+        current_player = "X"
+    end
+end
